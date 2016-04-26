40bf4f9c3a187a6dbe42ddacc6785fc5b4bc00cb
Chiot <jian_yingshuo@msn.com>
2016-04-26 20:38:35 +0800

    update

---
+++ pages/index/familyCard.js
@@ -0,0 +1,17 @@
+/**
+ * Created by jian_ on 2016/4/26.
+ */
+!function (window, document) {
+
+    var showFamilyCard = function () {
+
+    };
+
+    window.familyCard = {
+        init: function (option) {
+            if (option.show) {
+
+            }
+        }
+    }
+}(window, document);
